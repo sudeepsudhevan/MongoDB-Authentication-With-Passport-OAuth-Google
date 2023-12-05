018171c0e3e13a6f6425741e1c36406956b5273e
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-12-05 12:19:36 +0530

    Level 1 username and password

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,16 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
 
 
 app.get("/", (req, res) => {
@@ -22,7 +33,36 @@ app.get("/register", (req, res) => {
     res.render("register.ejs")
 })
 
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
 
+    newUser.save()
+        .then(() => {
+            res.render("secrets.ejs");
+        }).catch((err) => {
+            console.log(err);
+        });
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }).then((foundUser) => {
+        if (foundUser) {
+            if (foundUser.password === password) {
+                res.render("secrets.ejs");
+            }
+        }
+    }).catch((err) => {
+        console.log(err);
+    });
+
+
+})
 
 
 app.listen(port, () => {
