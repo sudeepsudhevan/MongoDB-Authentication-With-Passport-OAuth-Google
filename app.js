e41d1e234a187e1e63afd82ed8a15a3af87e22a3
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-12-07 12:04:45 +0530

    Level 5 Using passport ADD Cookies and Session

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-const saltRounds = 10;
+import session from "express-session"; // Step 1 import necessary packages
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
+
 
 const app = express();
 const port = 3000;
@@ -12,6 +14,15 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session({   // step 2 session with initial configration
+    secret: "It is a Secret",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize()); // step 3 Passport initialize
+app.use(passport.session());  // step 4 passport session
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({  // Add Schema from mongoose
@@ -19,8 +30,15 @@ const userSchema = new mongoose.Schema({  // Add Schema from mongoose
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose); // step 5 add passportlocalmongoose to UserSchema as plugin 
+// only works if it use mongoose.schema for the model creation/schema creation.   
+
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy()); // step 6  Creates a configured passport-local LocalStrategy instance that can be used in passport.
+
+passport.serializeUser(User.serializeUser()); // Create cookie
+passport.deserializeUser(User.deserializeUser()); //  find what in cookie
 
 app.get("/", (req, res) => {
     res.render("home.ejs")
@@ -34,42 +52,53 @@ app.get("/register", (req, res) => {
     res.render("register.ejs")
 })
 
-app.post("/register", (req, res) => {
+app.get("/secrets", (req, res) => { // step 8
+    if (req.isAuthenticated()) {
+        res.render("secrets.ejs")
+    } else {
+        res.redirect("/login")
+    }
+});
 
-    bcrypt.hash(req.body.password, saltRounds).then(function (hash) { // bcrypt salt + hash
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save()
-            .then(() => { // promise
-                res.render("secrets.ejs");
-            }).catch((err) => {
-                console.log(err);
-            });
-    }).catch((err) => {
-        console.log(err);
-    });
+app.get('/logout', function (req, res) { // step 10
+    req.logout(function (err) {
+        if (err) { return next(err); }
+    })
+
+    res.redirect('/'); // or wherever you want to redirect after logout
+});
+
+app.post("/register", (req, res) => { // step 7
+
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect('/secrets');
+            })
+        }
+    })
 });
 
 
-app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({ email: username }).then((foundUser) => {
-        if (foundUser) {
-            bcrypt.compare(password, foundUser.password).then(function (result) {
-                if (result == true) {
-                    res.render("secrets.ejs");
-                }
-            }).catch((err) => {
-                console.log(err);
-            });
-        }
-    }).catch((err) => {
-        console.log(err);
+app.post("/login", (req, res) => { // step 9
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
     });
+
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect('/secrets')
+            })
+        }
+    })
+
 })
 
 
